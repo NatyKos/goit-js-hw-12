e654a865a0131f3a42bb262c5a8e48dfd9ae6dbc
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-03-03 20:39:11 +0000

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export async function searchImages(query, page) {
-    // const KEY = '42530845-4f1978a0628226c655e2788d5';
-   
-
-    // const LINK = `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
     const baseURL = 'https://pixabay.com/api/';
     const response = await axios.get(baseURL, {
         params: {
@@ -16,12 +12,8 @@ export async function searchImages(query, page) {
             per_page: 15,
             page: page
         }
-    })
-        // .then(response => {
-        //     if (!response.ok) {
-        //         throw new Error(response.status)
-        //     }
-            return response.data
-        
-        }
+    }
+    )
+    return response.data
+}
 
